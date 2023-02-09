461bc2d32db87db28b1a964c064c28e0f3696825
Valtteri Raatikainen <valtteri.raatikainen1@gmail.com>
2023-02-09 14:37:08 +0200

    Booklist endpointin lisäys

--- src/main/java/com/example/Bookstore/web/BookController.java
+++ src/main/java/com/example/Bookstore/web/BookController.java
@@ -1,14 +1,26 @@
 package com.example.Bookstore.web;
 
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
+
+import com.example.Bookstore.domain.BookRepository;
 
 @Controller
 public class BookController {
 	@RequestMapping("/index")
 	public String IndexController() {
 		return "index.html";
+	} 
+	@Autowired
+	private BookRepository repository;
+	
+	@RequestMapping("/booklist")
+	public String BookList(Model model) {
+		model.addAttribute("books", repository.findAll());
+		return "booklist.html";
 	} 	
 }
+
