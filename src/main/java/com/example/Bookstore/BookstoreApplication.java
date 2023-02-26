e2e3e6c7c1188879a115f54928e79f540393e09f
Valtteri Raatikainen <valtteri.raatikainen1@gmail.com>
2023-02-26 22:05:29 +0200

    Lisätään kategoria kirjalistaan

--- src/main/java/com/example/Bookstore/BookstoreApplication.java
+++ src/main/java/com/example/Bookstore/BookstoreApplication.java
@@ -20,30 +20,25 @@ public class BookstoreApplication {
 	}
 
 	@Bean
-	public CommandLineRunner demo(BookRepository repository) {
+	public CommandLineRunner demo(BookRepository bookRepository, CategoryRepository categoryRepository) {
 		return (args) -> {
-			Book b1 = new Book("Testikirja", "Testi Testi", 1994, "392-4225-2354", 20.59);
-			Book b2 = new Book("Testikirja2", "Testi Testinen", 1995, "392-4225-1234", 22.29);
-			Book b3 = new Book("Testikirja3", "Testinen Testi", 1996, "392-4225-6789", 26.89);
 
-			repository.save(b1);
-			repository.save(b2);
-			repository.save(b3);
+			Category c1 = new Category("Scifi");
+			Category c2 = new Category("Rikos");
+			Category c3 = new Category("Seikkailu");
+			
+			categoryRepository.save(c1);
+			categoryRepository.save(c2);
+			categoryRepository.save(c3);
 
+			Book b1 = new Book("Testikirja", "Testi Testi", 1994, "392-4225-2354", 20.59, c1);
+			Book b2 = new Book("Testikirja2", "Testi Testinen", 1995, "392-4225-1234", 22.29, c2);
+			Book b3 = new Book("Testikirja3", "Testinen Testi", 1996, "392-4225-6789", 26.89, c3);
+
+			bookRepository.save(b1);
+			bookRepository.save(b2);
+			bookRepository.save(b3);
 		};
 	}
 
-	@Bean
-	public CommandLineRunner demo2(CategoryRepository repository) {
-		return (args) -> {
-			Category c1 = new Category(null, "Scifi");
-			Category c2 = new Category(null, "Rikos");
-			Category c3 = new Category(null, "Seikkailu");
-
-			repository.save(c1);
-			repository.save(c2);
-			repository.save(c3);
-
-		};
-	}
 }
--- src/main/java/com/example/Bookstore/domain/Book.java
+++ src/main/java/com/example/Bookstore/domain/Book.java
@@ -5,6 +5,8 @@ import jakarta.persistence.Entity;
 import jakarta.persistence.GeneratedValue;
 import jakarta.persistence.GenerationType;
 import jakarta.persistence.Id;
+import jakarta.persistence.JoinColumn;
+import jakarta.persistence.ManyToOne;
 
 @Entity
 public class Book {
@@ -17,14 +19,19 @@ public class Book {
 	private int year;
 	private String isbn;
 	private Double price;
+	@ManyToOne
+    @JoinColumn ( name = "categoryid")
+    private Category category;
 	
-	public Book(String title, String author, int year, String isbn, Double price) {
+
+	public Book(String title, String author, int year, String isbn, Double price, Category category) {
 		super();
 		this.title = title;
 		this.author = author;
 		this.year = year;
 		this.isbn = isbn;
 		this.price = price;
+		this.category = category;
 	}
 	
 	public Book() {}
@@ -66,5 +73,19 @@ public class Book {
 		this.price = price;
 	}
 	
-	
+	public Category getCategory() {
+		return category;
+	}
+
+	public void setCategory(Category category) {
+		this.category = category;
+	}
+
+	@Override
+    public String toString() {
+        if (this.category != null)
+            return "Book [id=" + id + ", title=" + title + ", author=" + author + ", year=" + year + "isbn=" + isbn + "price=" + price + " category=" + this.getCategory() + "]";
+        else
+            return "Book [id=" + id + ", title=" + title + ", author=" + author + ", year=" + year +  "isbn=" + isbn + "price=" + price + "]";
+	}
 }
